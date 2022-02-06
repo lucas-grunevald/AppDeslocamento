b3ba137a8b33c38a2dc38aad604fa533908141f8
lucas-grunevald <grunevaldlucas@gmail.com>
2022-02-06 20:10:17 -0300

    feature: get by id

---
+++ AppDeslocamento.Application/Clientes/Queries/GetClienteByIdQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using AppDeslocamento.Domain.Entities;
+using AppDeslocamento.Domain.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDeslocamento.Application.Clientes.Queries
+{
+    public class GetClienteByIdQuery : IRequest<Cliente>
+    {
+        public long clienteId { get; set; }
+    }
+
+    public class GetClienteByIdQueryHandler : IRequestHandler<GetClienteByIdQuery, Cliente>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetClienteByIdQueryHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Cliente> Handle(
+            GetClienteByIdQuery request,
+            CancellationToken cancellationToken)
+        {
+            var repository = _unitOfWork.GetRepository<Cliente>();
+
+            var cliente = await repository
+                .GetByIdAsync(request.clienteId);
+
+            return cliente;
+        }
+    }
+}
---
+++ AppDeslocamento.Application/Condutores/Queries/GetCondutorByIdQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using AppDeslocamento.Domain.Entities;
+using AppDeslocamento.Domain.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDeslocamento.Application.Condutores.Queries
+{
+    public class GetCondutorByIdQuery : IRequest<Condutor>
+    {
+        public long condutorId { get; set; }
+    }
+
+    public class GetCondutorByIdQueryHandler : IRequestHandler<GetCondutorByIdQuery, Condutor>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetCondutorByIdQueryHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Condutor> Handle(
+            GetCondutorByIdQuery request,
+            CancellationToken cancellationToken)
+        {
+            var repository = _unitOfWork.GetRepository<Condutor>();
+
+            var condutor = await repository
+                .GetByIdAsync(request.condutorId);
+
+            return condutor;
+        }
+    }
+}
